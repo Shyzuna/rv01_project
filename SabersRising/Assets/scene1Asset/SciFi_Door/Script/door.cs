7320f9b7e4802b86830031ee37c09d3e0b5b7f65
samshark <samuel.lounis@bbox.fr>
2015-11-10 15:40:34 +0100

    new commit

---
+++ SabersRising/Assets/scene1Asset/SciFi_Door/Script/door.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour {
+	public GameObject thedoor;
+
+	void OnTriggerEnter()
+	{
+		thedoor.GetComponent<Animation> ().Play ("open");
+	}
+	void OnTriggerExit()
+	{
+		thedoor.GetComponent<Animation> ().Play ("close");
+	}
+}
